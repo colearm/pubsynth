dfa8b26c04e90e31e86b92388d5ec056cfb48d14
colearm <arm.cole@outlook.com>
2023-05-24 21:02:35 -0500

    set up api calls to retrieve pmids/text and created a function to parse the medline data for abstracts

--- app.py
+++ app.py
@@ -24,7 +24,31 @@ def results():
     return render_template("results.html")
 
 
-response = requests.get(
-    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?term=does+deodorant+cause+breast+cancer&retmax=5&retmode=json&sort=relevance")
-pmids = response.json()['esearchresult']['idlist']
+def get_abstracts(medline):
+    ab, abstract, abstract_list = False, "", []
+    for line in medline.splitlines():
+        if line.startswith("AB"):
+            ab = True
+            abstract += line[6:]
+        elif ab and line.startswith("      "):
+            abstract += line[6:]
+        elif ab:
+            abstract_list.append(abstract)
+            ab, abstract = False, ""
+        else:
+            continue
+    return abstract_list
+
+
+query = input("Search query: ")
+esearch_params = {"db": "pubmed", "term": query, "retmax": "5", "retmode": "json", "sort": "relevance"}
+esearch = requests.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi", params=esearch_params)
+pmids = ','.join(esearch.json()['esearchresult']['idlist'])
+print("===========================")
 print(pmids)
+
+efetch_params = {"db": "pubmed", "id": pmids, "rettype": "medline", "retmode": "text"}
+efetch = requests.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi", params=efetch_params)
+abstracts = get_abstracts(efetch.text)
+print(abstracts)
+print("===========================")
