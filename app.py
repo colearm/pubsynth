49f6443d313ec36a93584f3ae905444584d20bad
colearm <arm.cole@outlook.com>
2023-05-25 21:44:07 -0500

    added call to gpt api to summarize abstracts pulled from pubmed

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import requests
+import os
+import openai
 
 app = Flask(__name__)
 
@@ -25,32 +27,58 @@ def results():
 
 
 def get_abstracts(medline):
-    ab, abstract, abstract_list = False, "", []
+    ab, tmp, abstracts = False, "", ""
     for line in medline.splitlines():
-        if line.startswith("AB"):
+        if line.startswith("AB"):  # reached the start of the abstract
             ab = True
-            abstract += line[6:]
-        elif ab and line.startswith("      "):
-            abstract += line[6:]
-        elif ab:
-            abstract_list.append(abstract)
-            ab, abstract = False, ""
-        else:
+            tmp += line[6:]
+        elif ab and line.startswith("      "):  # in the middle of the abstract
+            tmp += line[6:]
+        elif ab:  # reached the end of the abstract
+            abstracts += ("#####" + tmp)
+            ab, tmp = False, ""
+        else:  # haven't found start of next abstract yet
             continue
-    return abstract_list
+    return abstracts
 
 
 query = input("Search query: ")  # simulating search bar entry for now
+
 if query == "":
     exit()
-esearch_params = {"db": "pubmed", "term": query, "retmax": "5", "retmode": "json", "sort": "relevance"}
+
+
+esearch_params = {"db": "pubmed", "term": query, "retmax": "3", "retmode": "json", "sort": "relevance"}
 esearch = requests.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi", params=esearch_params)
 pmids = ','.join(esearch.json()['esearchresult']['idlist'])
 print("===========================")
-print(pmids)
+# print(pmids)
+# print("===========================")
+
 
 efetch_params = {"db": "pubmed", "id": pmids, "rettype": "medline", "retmode": "text"}
 efetch = requests.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi", params=efetch_params)
 abstracts = get_abstracts(efetch.text)
-print(abstracts)
+# print(abstracts)
+# print("===========================")
+
+
+openai.apikey = os.getenv("OPENAI_API_KEY")
+
+prompt = """Synthesize the key pieces of information from the following research paper abstracts into one coherent summary that is comprehensible to the average person. 
+Each abstract you are being given starts with #####. Use this to help you isolate the topics of each abstract to create a more intelligible summary.
+Your summary shold be around 10 sentences long: """
+"""
+gpt_response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+            "role": "assistant",
+            "content": prompt + abstracts
+        }
+    ]
+)
+result = gpt_response["choices"][0]["message"]["content"]
+print(result)
 print("===========================")
+"""
