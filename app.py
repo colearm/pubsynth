05759603509936c622fdf40cccd4ee199748ece8
colearm <arm.cole@outlook.com>
2023-06-02 21:14:28 -0500

    setup sqlite database + basic register/login/logout

--- app.py
+++ app.py
@@ -1,24 +1,93 @@
 from flask import Flask, render_template, request, redirect, session, Response
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 import requests
 import os
 import openai
 import pdfkit
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('PS_DB_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = os.getenv('PS_SECRET_KEY')
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
 openai.apikey = os.getenv("OPENAI_API_KEY")
 
 
-@app.route("/", methods=["POST", "GET"])
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader # loads users object from the user id stored in the session
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+
+class Users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"}) # different than pass length in db b/c pass will be hashed
+    submit = SubmitField("Register")
+    def validate_username(self, username): # check for duplicate username in db
+        existing_user = Users.query.filter_by(username=username.data).first()
+        if existing_user:
+            raise ValidationError("That username is already taken.")
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"}) # different than pass length in db b/c pass will be hashed
+    submit = SubmitField("Login")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_pass = bcrypt.generate_password_hash(form.password.data)
+        new_user = Users(username=form.username.data, password=hashed_pass)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect("/login")
+    return render_template("register.html", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect("/")
+    return render_template("login.html", form=form)
+
+
+@app.route("/logout", methods=["GET", "POST"])
+@login_required
+def logout():
+    logout_user()
+    return redirect("/login")
+
+
+@app.route("/", methods=["GET", "POST"])
+@login_required
 def search():
     if request.method == "POST":
-        query = request.form["query"]  # get search query from search bar
-        session["query"] = query
-        pmids = get_pmids(query)  # get pmids from search query
+        search_query = request.form["query"]  # get search query from search bar
+        session["search_query"] = search_query
+        pmids = get_pmids(search_query)  # get pmids from search query
         session["pmids"] = pmids
         titles = get_titles(pmids) # get titles from pmids
         session["titles"] = titles
@@ -29,13 +98,6 @@ def search():
         return redirect("/results")
     return render_template("search.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-@app.route("/register")
-def register():
-    return render_template("register.html")
 
 @app.route("/recents")
 def recents():
@@ -47,9 +109,9 @@ def favorites():
     return render_template("favorites.html")
 
 
-@app.route("/results", methods=["POST", "GET"])
+@app.route("/results", methods=["GET", "POST"])
 def results():
-    query = session["query"]  # query is the same regardless of level of detail
+    search_query = session["search_query"]  # query is the same regardless of level of detail
     pmids = session["pmids"] # pmids are the same regardless of level of detail
     titles = session["titles"] # titles are the same regardless of level of detail
     pmid_list = pmids.split(",")
@@ -57,17 +119,17 @@ def results():
         detail = 1 if "increase" in request.form else -1
         summary = get_summary(session["abstracts"], detail)
         session["result"] = summary  # update session variable
-        return render_template("results.html", query=query, result=summary, pmids=pmid_list, titles=titles)
+        return render_template("results.html", query=search_query, result=summary, pmids=pmid_list, titles=titles)
     result = session["result"]
-    return render_template("results.html", query=query, result=result, pmids=pmid_list, titles=titles)
+    return render_template("results.html", query=search_query, result=result, pmids=pmid_list, titles=titles)
 
 
 @app.route("/download")
 def download_pdf():
     pdf = generate_pdf()
     response = Response(pdf)
-    query = session["query"]
-    response.headers["Content-Disposition"] = f"attachment; filename={query}.pdf"
+    search_query = session["search_query"]
+    response.headers["Content-Disposition"] = f"attachment; filename={search_query}.pdf"
     response.mimetype = "application/pdf"
     return response
 
@@ -88,8 +150,8 @@ def parse_abstracts(medline):
     return abstracts
 
 
-def get_pmids(query):
-    esearch_params = {"db": "pubmed", "term": query, "retmax": "3", "retmode": "json", "sort": "relevance"}
+def get_pmids(search_query):
+    esearch_params = {"db": "pubmed", "term": search_query, "retmax": "3", "retmode": "json", "sort": "relevance"}
     esearch = requests.get(
         "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi", params=esearch_params)
     return ','.join(esearch.json()["esearchresult"]["idlist"])
@@ -140,7 +202,7 @@ def get_summary(abstracts, detail=0):
 
 
 def generate_pdf():
-    render = render_template("download.html", pmids=session["pmids"].split(","), query=session["query"], result=session["result"])
+    render = render_template("download.html", pmids=session["pmids"].split(","), query=session["search_query"], result=session["result"])
     options = {
         'page-size': 'Letter',
         'margin-top': '0.75in',
