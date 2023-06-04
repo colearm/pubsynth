aa0b6e8c74c4582fa2af4693d48e95cd4c666e71
colearm <arm.cole@outlook.com>
2023-06-03 19:11:14 -0500

    introduced forgot/reset password functionality w/ reset link sent to user's email

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, request, redirect, session, Response
+from flask import Flask, render_template, request, redirect, session, Response, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
+from flask_bcrypt import Bcrypt
+from flask_mail import Mail, Message
 from wtforms import EmailField, StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError, Email
-from flask_bcrypt import Bcrypt
 from datetime import timedelta
+from time import time
+import jwt
 import requests
 import os
 import openai
@@ -15,7 +18,14 @@ import pdfkit
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = os.getenv('PS_SECRET_KEY')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASS')
 db = SQLAlchemy(app)
+mail = Mail(app)
 bcrypt = Bcrypt(app)
 openai.apikey = os.getenv("OPENAI_API_KEY")
 
@@ -35,6 +45,21 @@ class Users(db.Model, UserMixin):
     email = db.Column(db.String(100), nullable=False, unique=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+
+    def generate_token(self, expiration=600): # reset link is valid for 10 minutes
+        return jwt.encode(
+            {"user_id": self.id, "exp": time() + expiration},
+            app.config['SECRET_KEY'],
+            algorithm='HS256'
+        )
+    
+    @staticmethod
+    def verify_token(token):
+        try:
+            user_id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['user_id']
+        except:
+            return
+        return Users.query.get(user_id)
 
 
 class RegisterForm(FlaskForm):
@@ -61,8 +86,21 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Log in")
 
 
+class ForgotForm(FlaskForm):
+    email = EmailField(validators=[InputRequired(), Length(max=100), Email(message="Enter a valid email address.", check_deliverability=True)], render_kw={"placeholder": "Email address"})
+    submit = SubmitField("Reset Password")
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "New Password"})
+    confirm_password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Confirm New Password"})
+    submit = SubmitField("Reset Password")
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect("/")
     form = RegisterForm()
     if form.validate_on_submit():
         hashed_pass = bcrypt.generate_password_hash(form.password.data)
@@ -75,6 +113,8 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect("/")
     form = LoginForm()
     if form.validate_on_submit():
         user = Users.query.filter_by(username=form.username.data).first()
@@ -84,8 +124,7 @@ def login():
             else:
                 login_user(user)
             return redirect("/")
-        else:
-            return render_template("login.html", form=form, invalid_login="Username or password is incorrect.")
+        return render_template("login.html", form=form, invalid="Username or password is incorrect.")
     return render_template("login.html", form=form)
 
 
@@ -94,6 +133,37 @@ def login():
 def logout():
     logout_user()
     return redirect("/login")
+
+
+@app.route("/forgot-password", methods=["GET", "POST"])
+def forgot_password():
+    if current_user.is_authenticated:
+        return redirect("/")
+    form = ForgotForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user:
+            send_reset_email(user)
+        return render_template("forgot.html", form=form, sent="If the email address you entered is associated with an account, you'll receive a link to reset your password shortly.")
+    return render_template("forgot.html", form=form)
+
+
+@app.route("/reset-password/<token>", methods=["GET", "POST"])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect("/")
+    user = Users.verify_token(token)
+    if not user:
+        return redirect("/forgot-password")
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        if form.password.data == form.confirm_password.data:
+            hashed_pass = bcrypt.generate_password_hash(form.password.data)
+            user.password = hashed_pass
+            db.session.commit()
+            return redirect("/login")
+        return render_template("reset.html", form=form, invalid="The passwords you entered do not match.")
+    return render_template("reset.html", form=form)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -149,6 +219,20 @@ def download_pdf():
     return response
 
 
+def send_reset_email(user):
+    token = user.generate_token()
+    msg = Message("PubSynth - Password Reset Request", sender=os.getenv('EMAIL_USER'), recipients=[user.email])
+    msg.body = f"""We have received a password reset request for your account.
+
+Visit the following link to reset your password: {url_for("reset_password", token=token, _external=True)}
+
+If you did not initiate a reset request, you may safely ignore this email.
+
+Thank you,
+The PubSynth Team
+    """
+    mail.send(msg)
+
 def parse_abstracts(medline):
     ab, tmp, abstracts = False, "", ""
     for line in medline.splitlines():
@@ -191,18 +275,18 @@ def get_abstracts(pmids):
 def get_summary(abstracts, detail=0):
     if detail == 0:
         prompt = """Your job is to synthesize the key pieces of information from the following research paper abstracts into one coherent summary that is comprehensible to the 
-        average person. Each abstract you are being given starts with #####. Use this to help you isolate the topics of each abstract to create a more intelligible summary. Your 
-        summary should be around 10 sentences long: """
+average person. Each abstract you are being given starts with #####. Use this to help you isolate the topics of each abstract to create a more intelligible summary. Your 
+summary should be around 10 sentences long: """
     elif detail == 1:
         summary = session["result"]
         prompt = f"""Your job is to synthesize the key pieces of information from the following research paper abstracts into one coherent summary that is comprehensible to the 
-        average person. The summary you gave last time was: "{summary}" Use details from the following abstracts to create a new summary with an increased level of detail and 
-        technicality. Your #1 priority is to make your response at least 20% longer than your last summary in terms of characters used. Each abstract begins with #####: """
+average person. The summary you gave last time was: "{summary}" Use details from the following abstracts to create a new summary with an increased level of detail and 
+technicality. Your #1 priority is to make your response at least 20% longer than your last summary in terms of characters used. Each abstract begins with #####: """
     else:
         summary = session["result"]
         prompt = f"""Your job is to synthesize the key pieces of information from the following research paper abstracts into one coherent summary that is comprehensible to the 
-        average person. The summary you gave last time was: "{summary}" Use the following abstracts to create a new, more generalized summary with a decreased level of detail and 
-        shorter response length compared to last time. Each abstract begins with #####: """
+average person. The summary you gave last time was: "{summary}" Use the following abstracts to create a new, more generalized summary with a decreased level of detail and 
+shorter response length compared to last time. Each abstract begins with #####: """
 
     gpt_response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
